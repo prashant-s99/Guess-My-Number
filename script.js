eb5148dcab5893dd0969dafbb18164e072054279
Prashant Sengar <64002012+prashant-s99@users.noreply.github.com>
2023-07-25 16:33:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,7 @@
 // console.log(document.querySelector(".guess").value);
 
 //Defining secret number
-let secNum =()=>{
- return(Math.trunc(Math.random() * 20) + 1);
-}
+let secNum =()=> Math.trunc(Math.random() * 20) + 1;
 let secretNumber=secNum();
 
 //Storing secret number in a variable which could be used later also.
@@ -22,36 +20,38 @@ const scr = Number(document.querySelector('.score').textContent);
 let score = scr;
 let highScore = Number(document.querySelector('.highscore').textContent);
 
+const showMessage = (message) => document.querySelector('.message').textContent = message;
+const setNumberText = (text) => document.querySelector('.number').textContent = text;
+
 //Main function which has all the conditions.
-let mainFunction = function () {
+let mainFunction = () => {
   let guess = Number(document.querySelector('.guess').value);
 
   if (score > 1 || guess === secretNumber || guess > 20) {
     if (!guess) {
-      document.querySelector('.message').textContent = 'Please enter a number!';
+      showMessage('Please enter a number!');
     } else if (guess === secretNumber) {
-      document.querySelector('.message').textContent = 'Correct Number! 🥳';
-      document.querySelector('.number').textContent = secretNumber;
+      showMessage('Correct Number! 🥳');
+      setNumberText(secretNumber);
       document.querySelector('body').style.backgroundColor = 'Green';
       if (score > highScore) {
         document.querySelector('.highscore').textContent = score;
       }
     } else if (guess < 0 || guess > 20) {
-      document.querySelector('.message').textContent =
-        'Number is not in range! 👎';
+      showMessage('Number is not in range! 👎');
     } else if (guess < secretNumber) {
-      document.querySelector('.message').textContent = 'Guess is too low. 📉';
+      showMessage('Guess is too low. 📉');
       score--;
       document.querySelector('.score').textContent = score;
     } else if (guess > secretNumber) {
-      document.querySelector('.message').textContent = 'Guess is too high. 📈';
+      showMessage('Guess is too high. 📈');
       score--;
       document.querySelector('.score').textContent = score;
     }
   } else if (score === 1 && guess != secretNumber) {
-    document.querySelector('.message').textContent = 'You Lost The Game!!!';
+    showMessage('You Lost The Game!!!');
     document.querySelector('.score').textContent = score - 1;
-    document.querySelector('.number').textContent = secretNumber;
+    setNumberText(secretNumber);
     document.querySelector('body').style.backgroundColor = 'Red';
   }
 };
@@ -61,14 +61,14 @@ document.querySelector('.check').addEventListener('click', mainFunction);
 
 //Will be invoked when 'Again' button is clicked.
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.number').textContent ='?';
+  setNumberText('?');
   score = scr;
+
+//Clearing Input field when clicked on Again button
   const guess = document.querySelector('.guess');
   const clear = document.querySelector('.again');
-  clear.addEventListener('click', () => {
-    guess.value = '';
-  });
-  document.querySelector('.message').textContent = 'Start guessing...';
+  clear.addEventListener('click', () => guess.value = '');
+  showMessage('Start guessing...');
   secretNumber = secNum();
   document.querySelector('body').style.backgroundColor = 'rgb(33, 34, 33';
   document.querySelector('.score').textContent = score;
@@ -78,17 +78,12 @@ document.querySelector('.again').addEventListener('click', function () {
 //Function to lock focus on the input element('guess').
 const inputField = document.getElementsByClassName('guess')[0];
 
-inputField.addEventListener('blur', event => {
-  event.target.focus();
-});
+inputField.addEventListener('blur', (event) => event.target.focus());
 
-document.addEventListener('click', event => {
-  const clickedElement = event.target;
-
+document.addEventListener('click', (event) => {
   // Check if the clicked element is not the input field
-  if (clickedElement !== inputField) {
+  if (event.target !== inputField) {
     event.preventDefault();
-
     // Focus the input field
     inputField.focus();
   }
